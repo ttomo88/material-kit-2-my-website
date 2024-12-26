e816b53f70f8006f4752d55fe361ef29fd9fb407
ttomo88 <107105500+ttomo88@users.noreply.github.com>
2024-12-26 01:01:51 +0100

    my first changes

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'core.urls'
 LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
 LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
-TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
+TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/home/templates/")  # ROOT dir for templates
 
 TEMPLATES = [
     {
